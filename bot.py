92d2a96f772635d01f34e84dc1c5750d9d9b52ea
Joe Lyu <68528026+Joe-Lyu@users.noreply.github.com>
2024-04-08 12:18:42 +0800

    added ability to guess entire word at once

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import pickle
+import re
 from utils import generate_keyword_game, format_game, make_guess
 TOKEN = pickle.load(open('token.pkl','rb')) #not gonna show my token to ya :)
 
@@ -29,7 +30,7 @@ class KeywordBot(discord.Client):
 
             def is_guess(msg):
                 guess = msg.content.lower()
-                if guess == '!quit':
+                if guess == '!quit' or re.match(r'^[a-zA-Z]{6}$', guess):
                     return True
                 try:
                     num, letter = guess.split(' ')
@@ -43,11 +44,28 @@ class KeywordBot(discord.Client):
                 guess = await self.wait_for('message', check=is_guess, timeout=600)
                 if guess.content == '!quit':
                     return await message.reply('Quit session.')
+                if len(guess.content) == 6:
+
+                    guesses -= 1
+                    for number, letter in enumerate(guess.content.lower()):
+                        hint_list, correct, guessed = make_guess([number+1,letter], hint_list, keyword)
+                        if correct and not guessed:
+                            guesses += 1
+                    if keyword == guess.content:
+                        await message.reply(format_game(hint_list))
+                        await message.reply("Nice guess!")
+                        break
+                    else:
+                        await message.reply(format_game(hint_list))
+                        await message.reply("Incorrect guess!")
+                        continue
+
                 guess = guess.content.lower().split(' ')
                 guess[0] = int(guess[0])
                 hint_list, correct, guessed = make_guess(guess, hint_list, keyword)
                 if guessed:
                     await message.reply("You have already guessed that letter correctly!")
+                    guesses -= 1
                 elif correct:
                     await message.reply("**Correct** letter!")
                 else:
